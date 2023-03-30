bace7350405695ea14db87df45de2facfa325913
Ben Hensor <benhensor@gmail.com>
2023-03-30 15:15:59 +0100

    more styles added, tried to improve the behaviour of the background image

---
+++ main.js
@@ -0,0 +1,8 @@
+// Change background-attachment when user scrolls
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    document.body.classList.add('scrolled');
+  } else {
+    document.body.classList.remove('scrolled');
+  }
+});
